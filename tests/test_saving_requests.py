190b3d5ea929c2caa5baabcc78be95d3572ad148
Chavithra PARANA <chavithra@gmail.com>
2023-03-23 14:21:04 +0100

    Example

--- tests/test_saving_requests.py
+++ tests/test_saving_requests.py
@@ -6,8 +6,14 @@ import requests
 
 # IMPORT INTERNAL
 
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [("User-Agent", None)],
+        "filter_query_parameters": [("apikey", "MOCK_API_KEY")],
+    }
 
 @pytest.mark.record_http
 def test_saving_requests():
-    response = requests.get("https://vcrpy.readthedocs.io/en/latest/configuration.html")
+    response = requests.get("https://vcrpy.readthedocs.io/en/latest/configuration.html?apikey=123")
     print(response.text)
